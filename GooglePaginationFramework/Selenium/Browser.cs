d63bee5a4b9e24c0a365c01bbc50afe5ed828f78
Vladislav <Vladislav@ENGINE>
2018-11-10 20:55:51 +0300

    Start project

---
+++ GooglePaginationFramework/Selenium/Browser.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace GooglePaginationFramework.Selenium
+{
+    public class Browser
+    {
+        //Initialize the webdriver
+        static IWebDriver webDriver = new FirefoxDriver();
+
+        static string Url = "https://www.google.com/intl/en/gmail/about/";
+
+        private static string HomePageTitle = "Gmail - Free Storage and Email from Google";
+        private static string LoginPageTitle = "Gmail";
+        private static string InboxPageTitle = "Inbox - testcsharp99@gmail.com - Gmail";
+
+
+        //Ask webdriver to open provided url
+        public static void GoToHomePage()
+        {
+            webDriver.Url = Url;
+        }
+
+        //Ask webdriver to get the title of the webpage
+        public static string Title
+        {
+            get { return webDriver.Title; }
+        }
+
+        //Ask webdriver to get webpage title and compare it to the home page title
+        public static bool IsAt()
+        {
+            return webDriver.Title == HomePageTitle;
+        }
+
+
+        //Find and click on the SignIn button
+        public static void GoToLoginPage()
+        {
+            var signIn = webDriver.FindElements(By.CssSelector(".gmail-nav__nav-link"))[1];
+            signIn.Click();
+        }
+
+
+        //Ask webdriver to get title and compare it to the login page title
+        public static bool IsAtLoginPage()
+        {
+            return webDriver.Title == LoginPageTitle;
+        }
+
+        //Login to account
+        public static void LoginToAccount()
+        {
+            //Find the login field and enter the data
+            var enterLogin = webDriver.FindElement(By.CssSelector("#identifierId"));
+            enterLogin.SendKeys("testmailcsharp99@gmail.com");
+
+            //Click "Next" button
+            var nextButton = webDriver.FindElement(By.CssSelector("#identifierNext > content:nth-child(3)"));
+            nextButton.Click();
+
+            System.Threading.Thread.Sleep(1000);
+
+            //Fing password field and enter password
+            var enterPassword = webDriver.FindElement(By.XPath("//div[@id='password']//input"));
+            enterPassword.SendKeys("NwmeJb235");
+
+            //Click "Next" button
+            var nextButton2 = webDriver.FindElement(By.CssSelector(".DL0QTb"));
+            nextButton2.Click();
+        }
+
+
+        //Ask webdriver get the title and compare it to the inbox title
+        public static bool IsAtInboxPage()
+        {
+            return webDriver.Title == InboxPageTitle;
+        }
+
+        //The function is closing the browser
+        public static void Close()
+        {
+            webDriver.Quit();
+        }
+    }
+}
---
+++ PaginationTests/LoginTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using GooglePaginationFramework;
+using GooglePaginationFramework.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaginationTests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void GoToTheHomePage()
+        {
+            //Get to the home page
+            Browser.GoToHomePage();
+
+            //Verify the browser is on the gmail home page
+            Assert.IsTrue(Browser.IsAt());
+        }
+
+        [TestMethod]
+        public void GoToTheLoginPage()
+        {
+            //Get to the login page
+            Browser.GoToLoginPage();
+
+            //Verify the browser is on the login page
+            Assert.IsTrue(Browser.IsAtLoginPage());
+        }
+
+        [TestMethod]
+        public void LoginToAccount()
+        {
+            //Get to the inbox
+            Browser.LoginToAccount();
+
+            //Verify the browser is on the inbox page
+            Assert.IsTrue(Browser.IsAtInboxPage());
+        }
+
+        /*[TestCleanup]
+        public void CleanUp()
+        {
+            //Close browser
+            Browser.Close();
+        }*/
+    }
+}
